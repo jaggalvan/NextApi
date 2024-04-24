51c04581d023ad6c466a383cf192ffd4133f3ad1
jaggalvan <jaggalvan@hotmail.com>
2024-04-24 17:28:49 -0600

    cuatro

--- components/pokemon/FavoritePokemons.tsx
+++ components/pokemon/FavoritePokemons.tsx
@@ -1,5 +1,5 @@
 import { Card, Grid } from '@nextui-org/react'
-import React from 'react'
+import React, { FC } from 'react'
 import FavoriteCardPokemon from './FavoriteCardPokemon'
 
 interface Props{
